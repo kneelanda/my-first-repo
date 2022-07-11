dbf1a5a2ddcf6f3bda9858ee62c250e427dfa684
Ashlin Kneeland <ashlin.jb.kneeland@gmail.com>
2022-07-10 18:17:34 -0700

    Added new python file text listed in assignment to the markdown file

---
+++ my_script.py
@@ -0,0 +1,10 @@
+# this is the "my_script.py" file...
+
+print("HELLO WORLD!")
+
+x = 2 + 2
+print(x)
+# This is just giberish and doesn't affect the script because it is behind a comment
+
+y=3*9
+print(y)
